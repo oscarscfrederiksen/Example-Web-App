0fc1ff0ec6aab460194260a69b3a0a68c2fd8e61
Oscar Frederiksen <oscar.frederiksen@me.com>
2021-01-29 11:00:50 +0000

    Edited Form.html and added formhandler.js

---
+++ JS/formhandler.js
@@ -0,0 +1,8 @@
+function submitEvent() {
+    console.log($('.form'));
+    //const form = document.querySelector('form');
+    //const data = new URLSearchParams(new FormData(form).entries());
+    //console.log(data);
+    //let assetObject = Object();
+    //localStorage.setItem(assetObject.name, JSON.stringify(assetObject));
+}
