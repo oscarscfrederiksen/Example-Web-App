8b18dac761a21f4a0e124574698719796b8f9c95
Oscar Frederiksen <oscar.frederiksen@me.com>
2021-01-29 14:43:43 +0000

    Edited form.html and formhandler.js

--- JS/formhandler.js
+++ JS/formhandler.js
@@ -1,8 +1,28 @@
-function submitEvent() {
-    console.log($('.form'));
+window.onload = function() {
+    const form = document.querySelector('.assetform');
+    form.addEventListener('submit', handleFormSubmit);
+}
+
+//function submitEvent() {
+    //console.log($('.form'));
     //const form = document.querySelector('form');
     //const data = new URLSearchParams(new FormData(form).entries());
     //console.log(data);
     //let assetObject = Object();
     //localStorage.setItem(assetObject.name, JSON.stringify(assetObject));
-}
+//}
+
+function handleFormSubmit(event) {
+    event.preventDefault();
+
+    const data = new FormData(event.target);
+    
+    console.log(data.AssetName)
+    console.log("ASSET DATA!")
+    //const formJSON = Object.fromEntries(data.entries());
+    
+    //console.log(JSON.stringify(formJSON, null, 2));
+    //const results = document.querySelector('.results pre');
+    //results.innerText = JSON.stringify(formJSON, null, 2);
+  }
+
